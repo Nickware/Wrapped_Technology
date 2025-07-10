32aeef2e937b688dcfa27f7306cec689be182de4
Nicolás Torres <naturalezacomputacional@gmail.com>
2025-07-09 22:27:28 -0500

    Python routine with cpython. Fibonacci program.

---
+++ cpython/script_fibonacci.py
@@ -0,0 +1,6 @@
+import fibonacci
+
+result = fibonacci.fibonacci(10)
+print(f"The 10th Fibonacci number is: {result}")
+result = fibonacci.fibonacci(20)
+print(f"The 20th Fibonacci number is: {result}")
